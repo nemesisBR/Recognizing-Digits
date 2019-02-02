6bab306bb07ef8746592efb132623ce2ccd3fc50
Boby Robert <bobyrobert3@gmail.com>
2019-02-02 23:33:43 +0530

    Used ANN
    imported keras
    
    Added 2 layers, but no output

--- digitRecog.py
+++ digitRecog.py
@@ -1,3 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+raw_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+labels = raw_data.iloc[:,0:1].values
+pixel_values = raw_data.iloc[:,1:785].values
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Convolution1D
+
+model = Sequential()
+
+model.add(Dense(units=16,kernel_initializer='uniform',activation='relu',input_dim = 784))
+
+model.add(Dense(units=16,activation='relu',init='uniform'))
+
+model.add(Dense(units=1,activation='relu',init='uniform'))
+
+model.compile(loss = 'mean_squared_error', optimizer = 'sgd')
+
+model.fit(pixel_values, labels,epochs=50,batch_size=20)
+
+label_pred = model.predict(test_data)
+
+
