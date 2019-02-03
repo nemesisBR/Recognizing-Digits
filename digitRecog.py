0ce56e87881aef7f3285c5979edc0ae15cdb39e3
Boby Robert <bobyrobert3@gmail.com>
2019-02-03 23:32:42 +0530

    Used ANN
    3 Layer model used but output labels were not as expected.
    
    Softmax activation not  done because of tensorflow verson error

--- digitRecog.py
+++ digitRecog.py
@@ -3,11 +3,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import keras
+
 
 raw_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 labels = raw_data.iloc[:,0:1].values
 pixel_values = raw_data.iloc[:,1:785].values
+
+labels = keras.utils.to_categorical(labels, num_classes = 10)
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -15,15 +19,15 @@ from keras.layers import Convolution1D
 
 model = Sequential()
 
-model.add(Dense(units=16,kernel_initializer='uniform',activation='relu',input_dim = 784))
+model.add(Dense(16,kernel_initializer='normal',activation='relu',input_dim = 784))
 
-model.add(Dense(units=16,activation='relu',init='uniform'))
+model.add(Dense(16,activation='relu',init='uniform'))
 
-model.add(Dense(units=1,activation='relu',init='uniform'))
+model.add(Dense(10,activation='sigmoid'))
 
-model.compile(loss = 'mean_squared_error', optimizer = 'sgd')
+model.compile(loss = 'categorical_crossentropy', optimizer = 'sgd')
 
-model.fit(pixel_values, labels,epochs=50,batch_size=20)
+model.fit(pixel_values, labels,epochs=150,batch_size=20)
 
 label_pred = model.predict(test_data)
 
